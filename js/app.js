18ae602f854e9f431f0e34b35989c6e9a0afba6f
FrancescoDOrtona <francescod.97@hotmail.it>
2023-11-07 01:35:43 +0900

    Add: js axios function to fetch emails & html str.

--- js/app.js
+++ js/app.js
@@ -5,9 +5,21 @@ console.log(axios)
 createApp ({
     data() {
         return {
-
+            message: 'Email List',
+			emailAddresses: [],
         }
     },
+    methods: {
+		fetchEmails(num) {
+			for (let i = 0; i < num; i++) {
+				axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+						const fetchedMail = res.data.response
+						this.emailAddresses.push(fetchedMail)
+					})
+			}
+		},
+	},
+	created() {
+		this.fetchEmails(10)
+	},
 }).mount('#app')
-
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
